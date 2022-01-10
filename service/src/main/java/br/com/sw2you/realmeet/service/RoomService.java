b9cba3880b0794204abea5b183a7acfb14d2524d
taiane94sb <taiane19sb94@gmail.com>
2022-01-10 20:29:14 -0300

    criando o RoomMapper com a API MapStruct

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -6,20 +6,22 @@ import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, RoomMapper roomMapper) {
         this.roomRepository = roomRepository;
+        this.roomMapper = roomMapper;
     }
 
     public RoomDTO getRoom(Long id) {
         requireNonNull(id);
         Room room = roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
-
-        return new RoomDTO().name(room.getName()).id(room.getId()).seats(room.getSeats());
+        return roomMapper.fromEntityToEntity(room);
     }
 }
