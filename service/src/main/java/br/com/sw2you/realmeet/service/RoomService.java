ba996041756f7b96ca6f9f6b4f5333c34701f7ec
taiane94sb <taiane19sb94@gmail.com>
2022-01-10 20:02:23 -0300

    criando a classe RoomService

---
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -0,0 +1,26 @@
+package br.com.sw2you.realmeet.service;
+
+import static java.util.Objects.requireNonNull;
+
+import br.com.sw2you.realmeet.domain.entity.Room;
+import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.exception.RoomNotFoundException;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public Room findById(Long id) {
+        requireNonNull(id);
+        return roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
+    }
+
+}
