a9bdad2f4e36c113c228d832e0a8899b50e0e5c0
taiane94sb <taiane19sb94@gmail.com>
2022-01-10 20:18:20 -0300

    montando o fluxo completo da busca de um Room pelo seu ID

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -2,25 +2,24 @@ package br.com.sw2you.realmeet.service;
 
 import static java.util.Objects.requireNonNull;
 
+import br.com.sw2you.realmeet.api.model.RoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
-import java.util.Objects;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoomService {
-
     private final RoomRepository roomRepository;
 
     public RoomService(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
-    public Room findById(Long id) {
+    public RoomDTO getRoom(Long id) {
         requireNonNull(id);
-        return roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
-    }
+        Room room = roomRepository.findById(id).orElseThrow(RoomNotFoundException::new);
 
+        return new RoomDTO().name(room.getName()).id(room.getId()).seats(room.getSeats());
+    }
 }
