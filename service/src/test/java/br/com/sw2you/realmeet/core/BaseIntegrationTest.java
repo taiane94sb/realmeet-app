65827bd6c8bcdb949b287c4a7e3685488d886ef7
taiane94sb <taiane19sb94@gmail.com>
2022-01-13 21:05:03 -0300

    outros cenários de teste de integração

--- service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/core/BaseIntegrationTest.java
@@ -4,12 +4,12 @@ import br.com.sw2you.realmeet.Application;
 import br.com.sw2you.realmeet.api.ApiClient;
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeEach;
 
 @ActiveProfiles(profiles = "integration-test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
--- service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import br.com.sw2you.realmeet.api.facade.RoomApi;
 import br.com.sw2you.realmeet.core.BaseIntegrationTest;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.HttpClientErrorException;
-import org.junit.jupiter.api.Test;
 
 class RoomApiIntegrationTest extends BaseIntegrationTest {
     @Autowired
@@ -36,5 +36,19 @@ class RoomApiIntegrationTest extends BaseIntegrationTest {
         assertEquals(room.getId(), dto.getId());
         assertEquals(room.getName(), dto.getName());
         assertEquals(room.getSeats(), dto.getSeats());
+    }
+
+    @Test
+    void testGetRoomInactive() {
+        var room = newRoomBuilder().active(false).build();
+        roomRepository.saveAndFlush(room);
+
+        assertFalse(room.getActive());
+        assertThrows(HttpClientErrorException.NotFound.class, () -> roomApi.getRoom(room.getId()));
+    }
+
+    @Test
+    void testGetRoomDoesNotExist() {
+        assertThrows(HttpClientErrorException.NotFound.class, () -> roomApi.getRoom(DEFAULT_ROOM_ID));
     }
 }
