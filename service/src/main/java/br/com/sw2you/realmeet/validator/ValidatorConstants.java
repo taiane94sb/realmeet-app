9007280eb90250ed2a49fc0bc4e6a2bda2709fc4
taiane94sb <taiane19sb94@gmail.com>
2022-01-19 21:23:51 -0300

    criando métodos comuns de validação no ValidatorUtils

---
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorConstants.java
@@ -0,0 +1,8 @@
+package br.com.sw2you.realmeet.validator;
+
+public final class ValidatorConstants {
+    public static final String MISSING = ".missing";
+    public static final String EXCEEDS_MAX_LENGTH = ".exceedsMaxLength";
+
+    private ValidatorConstants() {}
+}
--- service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
@@ -1,6 +1,10 @@
 package br.com.sw2you.realmeet.validator;
 
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import br.com.sw2you.realmeet.exception.InvalidRequestException;
+import java.util.Objects;
 
 public final class ValidatorUtils {
 
@@ -10,5 +14,34 @@ public final class ValidatorUtils {
         if (validatorErrors.hasErrors()) {
             throw new InvalidRequestException(validatorErrors);
         }
+    }
+
+    public static boolean validateRequired(String field, String fieldName, ValidatorErrors validatorErrors) {
+        if (isBlank(field)) {
+            validatorErrors.add(fieldName, fieldName + ValidatorConstants.MISSING);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateRequired(Object field, String fieldName, ValidatorErrors validatorErrors) {
+        if (isNull(field)) {
+            validatorErrors.add(fieldName, fieldName + ValidatorConstants.MISSING);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateMaxLength(
+        String field,
+        String fieldName,
+        int maxLength,
+        ValidatorErrors validatorErrors
+    ) {
+        if (!isBlank(field) && field.trim().length() > maxLength) {
+            validatorErrors.add(fieldName, fieldName + ValidatorConstants.EXCEEDS_MAX_LENGTH);
+            return true;
+        }
+        return false;
     }
 }
