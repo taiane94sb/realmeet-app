639ffa779dcbb6053035f43d81650f259087c366
taiane94sb <taiane19sb94@gmail.com>
2022-02-02 20:17:47 -0300

    atualizando a sala

--- service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
+++ service/src/main/java/br/com/sw2you/realmeet/service/RoomService.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 
 import br.com.sw2you.realmeet.api.model.CreateRoomDTO;
 import br.com.sw2you.realmeet.api.model.RoomDTO;
+import br.com.sw2you.realmeet.api.model.UpdateRoomDTO;
 import br.com.sw2you.realmeet.domain.entity.Room;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
@@ -41,6 +42,12 @@ public class RoomService {
     public void deleteRoom(Long roomId) {
         getActiveRoomOrThrow(roomId);
         roomRepository.deactivate(roomId);
+    }
+
+    @Transactional
+    public void updateRoom(Long roomId, UpdateRoomDTO updateRoomDTO) {
+        getActiveRoomOrThrow(roomId);
+        roomRepository.updateRoom(roomId, updateRoomDTO.getName(), updateRoomDTO.getSeats());
     }
 
     private Room getActiveRoomOrThrow(Long id) {
